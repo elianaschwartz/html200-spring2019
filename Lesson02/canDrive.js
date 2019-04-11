5272d459894343eec7b9c186957478a663addcc6
Eliana <etmschwartz@gmail.com>
2019-04-10 20:05:36 -0700

    adding candrive

---
+++ Lesson02/canDrive.js
@@ -0,0 +1,11 @@
+let first = 'Eliana'
+let last = 'Schwartz'
+let age = 14
+
+if (age >= 16) {
+  console.log(first + ' ' + last + ' ' + 'can drive', age-16 + ' ' + 'years' );
+} else {
+  for (let i = 5; i > 0; i--) {
+    console.log(i);
+  }
+}
