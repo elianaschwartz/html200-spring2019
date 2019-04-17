a692d734f79a430b87615ae28709f13621bd50a1
Eliana <etmschwartz@gmail.com>
2019-04-16 19:53:42 -0700

    adding assignments for lesson 3

---
+++ Lesson03/exercise-one.js
@@ -0,0 +1,15 @@
+let teams = ['Sounders', 'Timbers', 'Galaxy', 'Toronto FC', 'Red Bulls'];
+
+console.log(teams);
+
+teams.push('Rapids');
+
+console.log(teams);
+
+teams.splice(2, 1);
+
+console.log(teams);
+
+let str = teams.join(',');
+
+console.log(str);
---
+++ Lesson03/exercise-two.js
@@ -0,0 +1,5 @@
+let sent = 'The quick brown fox jumps over the lazy dog.';
+
+let words = sent.split(' ');
+
+console.log(words[2]);
