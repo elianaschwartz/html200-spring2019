b7f5cf0ac5ca48e8d347287ebf7b2900bdba29b6
Eliana <etmschwartz@gmail.com>
2019-04-21 15:48:40 -0700

    fixing mistakes

--- Lesson02/canDrive.js
+++ Lesson02/canDrive.js
@@ -3,7 +3,7 @@ let last = 'Schwartz';
 let age = 14;
 
 if (age >= 16) {
-  console.log(first + ' ' + last + ' ' + 'can drive', age-16 + ' ' + 'years' );
+  console.log(first + ' ' + last + ' ' + 'can drive' + age - 16 + ' ' + 'years');
 } else {
   for (let i = 5; i > 0; i--) {
     console.log(i);
